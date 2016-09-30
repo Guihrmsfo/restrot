5585f71e75bd7e666945a5c06c5addc2bc0d7e48
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-09-30 05:27:31 +0000

    register started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new 
+  end
+  def create
+    if (params[:user]).present?  
+        params.permit!
+        @user = User.new(params[:user])
+        if @user.save
+          flash[:notice] = "Cadastro com sucesso!"
+          flash[:color]= "valid"
+        else
+          flash[:notice] = "Dados inválidos"
+          flash[:color]= "invalid"
+        end
+    end
+    render "new"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  get 'register/index'
-
   get 'users/new'
+  
+  get 'users/create'
+  
+  post 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
