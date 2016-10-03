eb4dd8664c7f91d83762e86bbe641734e6665d82
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-03 19:46:16 +0000

    testes do controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,32 +11,40 @@ RSpec.describe UsersController, type: :controller do
   
   describe "GET #login" do
     it "user params not empty" do
+      get :login
       @user = UsersController.new
       expect(params[@user]).to be_present
     end
     
     it "user already exists" do
+      get :login
       @user = UsersController.new
-      @user_test = User.find_by name: @user.name
+      @user_test = User.find_by params[@user][:name]
+      #@user_test = User.find_by name: @user.name
       expect(@user_test).not_to be_nill
     end
   end
     
   describe "GET #create" do
+    get :create
     it "user params not empty" do
       @user = UsersController.new
       expect(params[@user]).to be_present
     end
     
     it "username does not exist" do
+      get :create
       @user = UsersController.new
-      @user_test = User.find_by name: @user.email
+      @user_test = User.find_by params[@user][:name]
+      #@user_test = User.find_by name: @user.name
       expect(@user_test).to be_nill
     end
     
     it "email does not exist" do
+      get :create
       @user = UsersController.new
-      @user_test = User.find_by email: @user.email
+      @user_test = User.find_by params[@user][:email]
+      #@user_test = User.find_by email: @user.email
       expect(@user_test).to be_nill
     end
     
