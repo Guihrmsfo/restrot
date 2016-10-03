4464a95bc15f6ed59076c0122bed5df59d5d7ce2
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-03 22:13:57 +0000

    testes do controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,43 +10,20 @@ RSpec.describe UsersController, type: :controller do
   end
   
   describe "GET #login" do
-    it "user params not empty" do
-      #user = UsersController.new
-      expect(UsersController.params[:user]).to be_present
-    end
-    
-    it "user already exists" do
+    it "is valid with valid atributes" do
       get :login
-      #@user = UsersController.new
-      @user_test = User.find_by UsersController.params[:user][:name]
-      #@user_test = User.find_by name: @user.name
-      expect(@user_test).not_to be_nill
+      user = UsersController.new
+      expect(user).to be_valid
     end
-  end
     
-  describe "GET #create" do
     it "user params not empty" do
-      get :create
-      #@user = UsersController.new
-      expect(UsersController.params[:user]).to be_present
+      get :login, params
+      expect(params[:name]).not_to be_empty
     end
     
-    it "username does not exist" do
-      get :create
-      #@user = UsersController.new
-      @user_test = User.find_by UsersController.params[:user][:name]
-      #@user_test = User.find_by name: @user.name
-      expect(@user_test).to be_nill
-    end
-    
-    it "email does not exist" do
-      get :create
-      #@user = UsersController.new
-      @user_test = User.find_by UsersController.params[:user][:email]
-      #@user_test = User.find_by email: @user.email
-      expect(@user_test).to be_nill
-    end
-    
+  end
+  
+  describe "GET #create" do
     it "returns http success" do
       get :create
       expect(response).to have_http_status(:success)
