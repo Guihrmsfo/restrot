07229ca8f151228ba536667d5e036891cf4a4894
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-04 03:38:56 +0000

    testes do model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,23 +10,10 @@ RSpec.describe UsersController, type: :controller do
   end
   
   describe "GET #login" do
-    it "is valid with valid atributes" do
-      get:login
-      user = UsersController.new
-      assert_not user.create
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
     end
-    
-    it "name not empty" do
-      get :login, :name
-      expect(:name).to be_present;
-    end
-    
-    it "user params not empty" do
-      get :login, :params
-      puts :params
-      expect([params]).to be_present
-    end
-    
   end
   
   describe "GET #create" do
