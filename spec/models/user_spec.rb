37d442ce4604f2f2bde1b7e27d2de6b0732e1790
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-04 07:23:33 +0000

    testes model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject {described_class.new(name: "Any Name", email: "email@123", password: "123456")}
   it "has valid attributes" do
-    expect(User.new(:name, :email, :password)).to be_valid
+    expect(subject).to be_valid
   end
 
   it "has empty email" do
-    u = User.new(:email=> nil)
-    expect(u).not_to be_valid
+    subject.email = nil
+    expect(subject).not_to be_valid
   end
   
   it "has empty name" do
-    u = User.new(:name=> nil)
-    expect(u).not_to be_valid
-  end
-  
-  it "has passwords that dont match" do
-    u = User.new(:password => "p1", :confirmation => "p2")
-    expect(u).not_to be_valid
+    subject.name = nil
+    expect(subject).not_to be_valid
   end
   
   it "username already taken" do
-    u = User.new(:name, :password, :email)
-    user_test = User.find_by name: u.name
+    user_test = User.find_by name: subject.name
     expect(user_test).to be_nill
   end
   
   it "email already taken" do
-    u = User.new(:email)
-    user_test = User.find_by name: u.email
+    user_test = User.find_by email: subject.email
     expect(user_test).to be_nill
   end
   
   it "password too short" do
-    u = User.new(:password)
-    expect(u.password).to have(6).characters
+    expect(subject.password).to have(6).characters
   end
 end
