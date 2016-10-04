8a1ab0df25e5e3d176b3cfbe4ef3f78625c2e214
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-04 08:06:29 +0000

    new routes on controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,21 +14,48 @@ RSpec.describe UsersController, type: :controller do
       it "returns http success" do
         post :login
         expect(response).to have_http_status(:success)
-     end
-     
-    #it is expected to redirect, but since Dashboard is not implemented yet, it returns the form again. So it does not pass the test. 
-     #it "redirect to dashboard" do
-     #  post :login
-     #  expect(response).to redirect_to(:controller => 'dashboard')
-     #end
+      end
+      
+      #it is expected to redirect, but since Dashboard is not implemented yet, it does not pass the test. 
+      #it "redirect to dashboard" do
+      #  post :login
+      #  expect(response).to redirect_to(:controller => 'dashboard')
+      #end
+    end
+ 
+    context "with invalid attributes" do
     end
   end
   
   describe "POST #create" do
     context "with valid attributes" do
+      before {post :create, :user => {:name => "Any Name", :email => "email@123", :password => "Password"} }
+      
       it "returns http success" do
-        post :create
         expect(response).to have_http_status(:success)
+      end
+      
+      #it is expected to redirect, but since Dashboard is not implemented yet, it does not pass the test. 
+      #it "redirect to dashboard" do
+      #  post :login
+      #  expect(response).to redirect_to(:controller => 'dashboard')
+      #end
+    end
+    
+    context "with invalid attributes" do
+      it "is missing params" do
+        post :create, :user => {:name => "", :email => "email@123", :password => "Password"}
+        expect(response).to render_template("login")
+      end
+      
+     it "is missing params" do
+        post :create, :user => {:name => "Any Name", :email => "", :password => "Password"}
+        expect(response).to render_template("login")
+      end
+      
+      it "is missing params" do
+        post :create, :user => {:name => "Any Name", :email => "email@123", :password => ""}
+        expect(response).to render_template("login")
       end
     end
   end
