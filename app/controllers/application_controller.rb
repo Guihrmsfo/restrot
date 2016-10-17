0137cd2fc05cf7873116e9357d6a03756ac07944
Gabriel Pioto <gabriel.pioto07@hotmail.com>
2016-10-16 22:50:15 -0200

    Adicionado o novo template (utilizando a gem 'adminlte2-rails')

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Uncomment next line to apply the adminLTE template every-f***ing-where
+  # layout 'admin_lte_2'
+
   protect_from_forgery with: :exception
 end
