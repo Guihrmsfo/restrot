caf18496dd45b4ae95a94bad0b823967f2697c41
Lucas Albero <lucasalbero96@gmail.com>
2016-10-17 19:22:36 +0000

    Modificar senha funcionando

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,23 +2,27 @@ require 'bcrypt'
 class PreferencesController < ApplicationController
 include BCrypt
 
-
+  
     def new
         @user = User.new
     end
     
     def password
-        @user =User.find(session[:user_id])
+        @user = User.find(session[:user_id])
         puts @user.name
-        if (params[:oldPassword]).present?
-            @password = BCrypt::Engine.hash_secret(params[@user.name][:oldPassword], @user.salt)
+        if (params[:user]).present?
+            @password = BCrypt::Engine.hash_secret(params[:user][:oldPassword], @user.salt)
             if @password == @user.password
-                if :password == :password_confirmation && @user.update_with_password(:password)
-                    bypass_sign_in(@user)
-                    redirect_to root_path
-                    flash[:notice] = "Senha alterada com sucesso!"
-                    flash[:color]= "Válido"
-                    return
+                if params[:user][:password] == params[:user][:passwordConfirmation]
+                    
+                    @user.password = params[:user][:password]
+                    if @user.save
+                        flash[:notice] = "Senha alterada com sucesso!"
+                        flash[:color]= "Válido"
+                    else
+                        flash[:notice] = "Erro!"
+                        flash[:color]= "Inválido"
+                    end
                 else
                     flash[:notice] = "Senha e confirmação de senha não são iguais"
                     flash[:color]= "Válido"
@@ -28,7 +32,7 @@ include BCrypt
                 flash[:color]= "Inválido"
             end
         else
-            flash[:notice] = "campo vazio"
+            flash[:notice] = "Campo vazio"
             flash[:color]= "Inválido"
         end
        # render "edit" - falta o template
--- spec/controllers/preferences_controller_spec.rb
+++ spec/controllers/preferences_controller_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe PreferencesController, type: :controller do
 
 
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
   describe "POST #password" do
     context "with valid attributes" do
-      before {post :update_password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "passwordConfirmation", :password => "Password"} }
+      before {post :password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "passwordConfirmation", :password => "Password"} }
       
       it "returns http success" do
         expect(response).to have_http_status(:success)
@@ -15,18 +22,18 @@ RSpec.describe PreferencesController, type: :controller do
     
     context "with invalid attributes" do
       it "is missing params" do
-        post :update_password, :user => {:oldPassword => "", :passwordConfirmation => "passwordConfirmation", :password => "Password"}
-        expect(response).to render_template("users/edit")
+        post :password, :user => {:oldPassword => "", :passwordConfirmation => "passwordConfirmation", :password => "password"}
+        expect(response).to render_template("preferences/password")
       end
       
      it "is missing params" do
-        post :update_password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "", :password => "Password"}
-        expect(response).to render_template("users/edit")
+        post :password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "", :password => "password"}
+        expect(response).to render_template("preferences/password")
       end
       
       it "is missing params" do
-        post :update_password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "passwordConfirmation", :password => ""}
-        expect(response).to render_template("users/edit")
+        post :password, :user => {:oldPassword => "oldPassword", :passwordConfirmation => "passwordConfirmation", :password => ""}
+        expect(response).to render_template("preferences/password")
       end
     end
   end
