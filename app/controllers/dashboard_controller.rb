323313366f814a6df55f268ba6a804ee5bc4f4b6
Gabriel Pioto <gabriel.pioto07@hotmail.com>
2016-10-17 00:49:33 -0200

    Layout da home arrumado Adicionada nova tela de login (novo template) Adicionado novo dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,11 @@
 class DashboardController < ApplicationController
+  layout 'admin_lte_2'
   def dashboard
-    
+
     #if session[:user_id].nil?
-     #   redirect_to :controller => 'users', :action => 'login' 
+     #   redirect_to :controller => 'users', :action => 'login'
       #  return
     #end
-    
+
   end
 end
