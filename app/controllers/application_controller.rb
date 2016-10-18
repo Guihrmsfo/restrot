241e2c0fdea154d7ddec314bd3bd319a08382644
Joao Armando <joaoarmando94@gmail.com>
2016-10-18 20:21:14 +0000

    Insere o template basico do dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   # Uncomment next line to apply the adminLTE template every-f***ing-where
   # layout 'admin_lte_2'
+  
+  helper_method :current_user
 
+  private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
   protect_from_forgery with: :exception
 end
