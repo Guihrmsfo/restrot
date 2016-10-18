3e8e0eea0508bea52b6d5052f6db49f803b48b95
Laura Castro Vieira <lauravieira.lvl@gmail.com>
2016-10-18 01:41:13 +0000

    encriptando senha ao alterar e limitando-a

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -11,11 +11,13 @@ include BCrypt
         @user = User.find(session[:user_id])
         puts @user.name
         if (params[:user]).present?
+            puts params[:user][:oldPassword]
             @password = BCrypt::Engine.hash_secret(params[:user][:oldPassword], @user.salt)
             if @password == @user.password
                 if params[:user][:password] == params[:user][:passwordConfirmation]
-                    
+                    @user.salt = nil
                     @user.password = params[:user][:password]
+                   
                     if @user.save
                         flash[:notice] = "Senha alterada com sucesso!"
                         flash[:color]= "Válido"
@@ -25,15 +27,12 @@ include BCrypt
                     end
                 else
                     flash[:notice] = "Senha e confirmação de senha não são iguais"
-                    flash[:color]= "Válido"
+                    flash[:color]= "Invalido"
                 end
             else
                 flash[:notice] = "Senha inválida"
                 flash[:color]= "Inválido"
             end
-        else
-            flash[:notice] = "Campo vazio"
-            flash[:color]= "Inválido"
         end
        # render "edit" - falta o template
     end
