64587f19bdc6fba5bb844ba8183f943194a483b6
Lucas Correa Silva <lucas.correa.silvaa@gmail.com>
2016-10-18 15:06:05 +0000

    Recuperação de Senha (só falta testar com o log out)

--- app/models/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-    default :from => "noreplyrestrot@gmail.com"
-
-    def registration_confirmation(user)
-        @user = user
-        mail(:to => "#{user.name} <#{user.email}>", :subject => "Confirmação de Cadastro")
-    end
-    
-end    
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
         get :confirm_email
     end
   end
+  
+  resources :password_resets
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
