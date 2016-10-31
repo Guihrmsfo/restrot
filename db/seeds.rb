59aae5cd018d821ed43c476929fb18af943c44f9
Lucas Correa Silva <lucas.correa.silvaa@gmail.com>
2016-10-31 20:04:46 +0000

    Pré-Cadastro de Itens e alguns Usuários no seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,29 @@
-Ingredient.create(name: 'Arroz Integral', unity_of_measure: 'Kilo')
+# USUÁRIOS 
+User.create(name: 'gpioto', email: 'gabriel.piotogomes@gmail.com')
+User.create(name: 'lucascorrea', email: 'lucascorreasilvaa@gmail.com')
+
+# INGREDIENTES
+Ingredient.create(name: 'Ovo', unity_of_measure: 'Unidade')
 Ingredient.create(name: 'Tomate', unity_of_measure: 'Unidade')
+Ingredient.create(name: 'Laranja', unity_of_measure: 'Unidade')
+Ingredient.create(name: 'Maçã', unity_of_measure: 'Unidade')
+Ingredient.create(name: 'Orégano', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Arroz Integral', unity_of_measure: 'Grama')
 Ingredient.create(name: 'Manjericão', unity_of_measure: 'Grama')
-Ingredient.create(name: 'Peito de Frango', unity_of_measure: 'Kilo')
+Ingredient.create(name: 'Peito de Frango', unity_of_measure: 'Grama')
 Ingredient.create(name: 'Alecrim', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Bacon', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Leite Condensado', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Chocolate Granulado', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Achocolatado', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Creme de Leite', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Coco Ralado', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Queijo Ralado', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Chocolate em Pó', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Azeitona', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Farinha de Trigo', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Fermento', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Alecrim', unity_of_measure: 'Grama')
+Ingredient.create(name: 'Leite', unity_of_measure: 'Mililitros')
+
+#LISTA DE INGREDIENTES
