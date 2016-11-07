951368f8886ff901074b567e667d2114a5348b64
Lucas Correa Silva <lucas.correa.silvaa@gmail.com>
2016-11-07 20:03:38 +0000

    Interação LISTA->Tela de Edição funcionando

--- app/controllers/ingredients_users_controller.rb
+++ app/controllers/ingredients_users_controller.rb
@@ -1,6 +1,7 @@
 class IngredientsUsersController < SessionController
   layout 'admin_lte_2'
   def edit
+    @ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).where("ingredient_id = ?", params[:id]).select("*")
   end
 
   def update
