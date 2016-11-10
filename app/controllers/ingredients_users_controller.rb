22dfd736266ad95aa29916bad67302bdd2fd271e
Lucas Correa Silva <lucas.correa.silvaa@gmail.com>
2016-11-10 19:26:48 +0000

    Atualização do Controller

--- app/controllers/ingredients_users_controller.rb
+++ app/controllers/ingredients_users_controller.rb
@@ -1,9 +1,6 @@
 class IngredientsUsersController < SessionController
   layout 'admin_lte_2'
   def edit
-    @ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).where("ingredient_id = ?", params[:id]).select("*")
-  end
-
-  def update
+    @ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).where("ingredient_id = ?", params[:id]).select("*").first
   end
 end
--- spec/controllers/ingredients_users_controller_spec.rb
+++ spec/controllers/ingredients_users_controller_spec.rb
@@ -3,17 +3,11 @@ require 'rails_helper'
 RSpec.describe IngredientsUsersController, type: :controller do
 
   describe "GET #edit" do
+    
     it "returns http success" do
       get :edit
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  
 end
