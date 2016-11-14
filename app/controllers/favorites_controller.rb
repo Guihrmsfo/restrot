05b591cd4b97343372a54a94ac4096ad474cd664
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-11-14 23:29:50 +0000

    View for favorites started

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < SessionController
   layout 'admin_lte_2'
   def index
+    @recipes = Recipe.joins(:favorite_recipes).where("user_id = ?", current_user.id).select("*")
   end
 end
