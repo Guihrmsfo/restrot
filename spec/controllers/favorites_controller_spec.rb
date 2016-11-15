ba1d73bed7550ac42575527bac013f617b73ca9e
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-11-15 04:13:03 +0000

    rspec for favorite recipes

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe FavoritesController, type: :controller do
     
     before(:each) do
       @password = '123456'
-      @user = User.new(id: 1, name: "Any Name", email: "anyemaik@gmail.com", password: @password)
+      @user = User.new(id: 1, name: "Any Name", email: "anyemail@gmail.com", password: @password)
       @user.save
       session[:user_id] = 1
     end
@@ -16,6 +16,23 @@ RSpec.describe FavoritesController, type: :controller do
         get :index
         expect(response).to have_http_status(:success)
       end
-    end  
+    end
+    
+    context "when user has favorites" do
+      before(:each) do
+        @favorite_recipe = FavoriteRecipe.create(user_id: 1, uri: "http://www.edamam.com/ontologies/edamam.owl#recipe_637913ec61d9da69eb451818c3293df2")
+        @favorite_recipe.save
+        session[:user_id] = 1
+      end
+      
+      describe "GET #index" do
+        it "finds favorites" do
+          get :index
+          expect(:favorites).to_not be_nil
+        end
+      end
+    
+    end
+    
   end
 end
