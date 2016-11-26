be884c38f0dcdfb85410c72bd0634bcced6b842e
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-11-26 19:32:09 +0000

    ingredients and favorites statistics

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < SessionController
   layout 'admin_lte_2'
   
   def dashboard
+    @ingredients_count = IngredientsUser.where(:user_id => session[:user_id]).count
+    @favorites_count = FavoriteRecipe.where(:user_id => session[:user_id]).count
     @ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).select("*").last(4)
   end
   
