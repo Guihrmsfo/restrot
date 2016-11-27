7987f5ae8b33bf78828f561cfac8889f0b72d5b4
Lucas Albero <lucasalbero96@gmail.com>
2016-11-27 16:41:36 +0000

    Testes no Travis arrumados

--- app/controllers/ingredients_users_controller.rb
+++ app/controllers/ingredients_users_controller.rb
@@ -2,7 +2,7 @@ class IngredientsUsersController < SessionController
   layout 'admin_lte_2'
   def edit
     @ing_id = IngredientsUser.where("id = ?", params[:id]).select("ingredient_id").first
-    @ingredient_in_ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).where("ingredient_id = ?", @ing_id.ingredient_id).select("*").first
+    @ingredient_in_ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).where("ingredient_id = ?", @ing_id[:ingredient_id]).select("*").first
     session[:return_to] ||= request.referer
   end
 end
