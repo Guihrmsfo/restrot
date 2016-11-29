dc622257c6fb44952f5c6bc516242ec718d127d0
Guilherme de Oliveira Fernandes <guilherme_oliveira55@hotmail.com>
2016-11-29 14:58:39 +0000

    fixes login count statistics

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,8 @@ class DashboardController < SessionController
     @ingredients_count = IngredientsUser.where(:user_id => session[:user_id]).count
     @favorites_count = FavoriteRecipe.where(:user_id => session[:user_id]).count
     @ingredients = Ingredient.joins(:ingredients_users).where("user_id = ?", session[:user_id]).select("*").last(4)
-    @usuario = User.find_by("id = ?", session[:user_id])
+    @user = current_user
+    puts @user
   end
   
 end
